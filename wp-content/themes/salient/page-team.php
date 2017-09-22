34ddbf4e87f2ba4372f0a622dae8cdbfaecf2412
BruceTaheri <taheri_bruce@mail.com>
2017-09-22 17:37:02 +0200

    add team management page, deploy to staging server

--- wp-content/themes/salient/page-team.php
+++ wp-content/themes/salient/page-team.php
@@ -3,8 +3,149 @@
 get_header(); ?>
 
 <div class="team_top">
-    <p></p>
-    <p></p>
+    <p>Team & Leadership -</p>
+    <p>Lorem ipsum dolor sit amet, est et esse moderatius, ne mei fugit semper impedit. 
+        Ea oportere necessitatibus vim. An vide mundi dissentiet vis, ut sit dico inani ceteros.</p>
 </div>
+<div class="team_section">
+    <p>Managing Consultant -</p>
+    <div class="team_img">
+        <div>
+            <img src="<?php echo $upload[baseurl] .'/wp-content/uploads/2017/09/team1.png'?>" alt="">
+            <p>Mike Geller</p>
+        </div>
+        <div>
+            <img src="<?php echo $upload[baseurl] .'/wp-content/uploads/2017/09/team1.png'?>" alt="">
+            <p>Brandi Starr</p>
+        </div>
+        <div>
+            <img src="<?php echo $upload[baseurl] .'/wp-content/uploads/2017/09/team1.png'?>" alt="">
+            <p>Ali Head</p>
+        </div>
+    </div>
 
+    <p>Senior Solutions Architect -</p>
+    <div class="team_img">
+        <div>
+            <img src="<?php echo $upload[baseurl] .'/wp-content/uploads/2017/09/team1.png'?>" alt="">
+            <p>Jesse Nobbe</p>
+        </div>
+        <div>
+            <img src="<?php echo $upload[baseurl] .'/wp-content/uploads/2017/09/team2.png'?>" alt="">
+            <p></p>
+        </div>
+        <div>
+            <img src="<?php echo $upload[baseurl] .'/wp-content/uploads/2017/09/team2.png'?>" alt="">
+            <p></p>
+        </div>
+    </div>
+
+    <p>Solutions Consultant -</p>
+    <div class="team_img">
+        <div>
+            <img src="<?php echo $upload[baseurl] .'/wp-content/uploads/2017/09/team1.png'?>" alt="">
+            <p>Andrew Kolbuc</p>
+        </div>
+        <div>
+            <img src="<?php echo $upload[baseurl] .'/wp-content/uploads/2017/09/team1.png'?>" alt="">
+            <p>Sheroy Mistry</p>
+        </div>
+        <div>
+            <img src="<?php echo $upload[baseurl] .'/wp-content/uploads/2017/09/team2.png'?>" alt="">
+            <p></p>
+        </div>
+    </div>
+
+    <p>Marketing Automation Consultant -</p>
+    <div class="team_img">
+        <div>
+            <img src="<?php echo $upload[baseurl] .'/wp-content/uploads/2017/09/team1.png'?>" alt="">
+            <p>Andrew Dunlap</p>
+        </div>
+        <div>
+            <img src="<?php echo $upload[baseurl] .'/wp-content/uploads/2017/09/team1.png'?>" alt="">
+            <p>Nadim Fetaih</p>
+        </div>
+        <div>
+            <img src="<?php echo $upload[baseurl] .'/wp-content/uploads/2017/09/team1.png'?>" alt="">
+            <p>Tim Happ</p>
+        </div>
+    </div>
+    <div class="team_img">
+        <div>
+            <img src="<?php echo $upload[baseurl] .'/wp-content/uploads/2017/09/team1.png'?>" alt="">
+            <p>Jessica Jones</p>
+        </div>
+        <div>
+            <img src="<?php echo $upload[baseurl] .'/wp-content/uploads/2017/09/team1.png'?>" alt="">
+            <p>Mackenzie Rose</p>
+        </div>
+        <div>
+            <img src="<?php echo $upload[baseurl] .'/wp-content/uploads/2017/09/team2.png'?>" alt="">
+            <p></p>
+        </div>
+    </div>
+
+    <p>Senior Associate Consultant -</p>
+    <div class="team_img">
+        <div>
+            <img src="<?php echo $upload[baseurl] .'/wp-content/uploads/2017/09/team1.png'?>" alt="">
+            <p>Mika Carter</p>
+        </div>
+        <div>
+            <img src="<?php echo $upload[baseurl] .'/wp-content/uploads/2017/09/team1.png'?>" alt="">
+            <p>Patrick Galapon</p>
+        </div>
+        <div>
+            <img src="<?php echo $upload[baseurl] .'/wp-content/uploads/2017/09/team1.png'?>" alt="">
+            <p>Rebecca Luong</p>
+        </div>
+    </div>
+    <div class="team_img">
+        <div>
+            <img src="<?php echo $upload[baseurl] .'/wp-content/uploads/2017/09/team1.png'?>" alt="">
+            <p>Mythili Viswanathan</p>
+        </div>
+        <div>
+            <img src="<?php echo $upload[baseurl] .'/wp-content/uploads/2017/09/team2.png'?>" alt="">
+            <p></p>
+        </div>
+        <div>
+            <img src="<?php echo $upload[baseurl] .'/wp-content/uploads/2017/09/team2.png'?>" alt="">
+            <p></p>
+        </div>
+    </div>
+
+    <p>Associate Consultant -</p>
+    <div class="team_img">
+        <div>
+            <img src="<?php echo $upload[baseurl] .'/wp-content/uploads/2017/09/team1.png'?>" alt="">
+            <p>Jessica Venerus</p>
+        </div>
+        <div>
+            <img src="<?php echo $upload[baseurl] .'/wp-content/uploads/2017/09/team2.png'?>" alt="">
+            <p></p>
+        </div>
+        <div>
+            <img src="<?php echo $upload[baseurl] .'/wp-content/uploads/2017/09/team2.png'?>" alt="">
+            <p></p>
+        </div>
+    </div>
+
+    <p>Operations -</p>
+    <div class="team_img">
+        <div>
+            <img src="<?php echo $upload[baseurl] .'/wp-content/uploads/2017/09/team1.png'?>" alt="">
+            <p>Sarah Little</p>
+        </div>
+        <div>
+            <img src="<?php echo $upload[baseurl] .'/wp-content/uploads/2017/09/team1.png'?>" alt="">
+            <p>Ainsley Berry</p>
+        </div>
+        <div>
+            <img src="<?php echo $upload[baseurl] .'/wp-content/uploads/2017/09/team2.png'?>" alt="">
+            <p></p>
+        </div>
+    </div>
+</div>
 <?php get_footer(); ?>
